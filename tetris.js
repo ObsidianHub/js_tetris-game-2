dbf787c9076aa1b78284f37b8c1ba4d914200f77
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-17 13:29:00 +0200

    create base vartiables

---
+++ tetris.js
@@ -0,0 +1,7 @@
+const cvs = document.getElementById("tetris");
+const ctx = cvs.getContext("2d");
+
+const ROW = 20;
+const COL = (COLUMN = 10);
+const SQ = (squareSize = 20);
+const VACANT = "WHITE"; // color of an empty square
