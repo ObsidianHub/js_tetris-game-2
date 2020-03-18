cbb8f4b522305da1ee9f46a1d3681cc2cf0e46ec
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-18 23:47:13 +0200

    create controls for piece

--- tetris.js
+++ tetris.js
@@ -113,6 +113,21 @@ Piece.prototype.rotate = function() {
   this.draw();
 };
 
+// CONTROL the piece
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event) {
+  if (event.keyCode == 37) {
+    p.moveLeft();
+  } else if (event.keyCode == 38) {
+    p.rotate();
+  } else if (event.keyCode == 39) {
+    p.moveRight();
+  } else if (event.keyCode == 40) {
+    p.moveDown();
+  }
+}
+
 // drop the piece every 1 sec
 let dropStart = Date.now();
 function drop() {
